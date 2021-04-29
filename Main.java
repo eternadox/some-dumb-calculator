008ee5056af0779281a199a37487fbc0451fae90
khosraw <khosraw.azizi@gmail.com>
2021-04-29 19:46:40 -0400

    added salary calculation option to Main

--- Main.java
+++ Main.java
@@ -1,10 +1,23 @@
 import event.calcMenu;
+import java.util.Scanner;
 
 public class Main {
 
   public static void main(String[] args) {
 
-    calcMenu.Main();
+    Scanner input = new Scanner(System.in);
+    System.out.println("Which program would you like to run?\n\n" +
+            "1. Simple Calculator\n" +
+            "2. Salary Calculator");
+    int option = input.nextInt();
+
+    if (option == 1) {
+      calcMenu.Main();
+
+    } else if (option == 2) {
+      Salary.calculate();
+
+    }
 
   }
 }
