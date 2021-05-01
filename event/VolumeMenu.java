d604dedbdcc6c8e1e1e649a04711b66564948f04
khosraw <khosraw.azizi@gmail.com>
2021-05-01 00:06:40 -0400

    Added sphere and polygonal pyramid

--- event/VolumeMenu.java
+++ event/VolumeMenu.java
@@ -9,16 +9,16 @@ public class VolumeMenu {
 
         System.out.println("What would you like to calculate the volume of?\n\n" +
                 "1. Cube\n" +
-                "2. Triangular Pyramid\n" +
-                "3. Square Pyramid\n" +
-                "4. Polygonal Prism");
+                "2. Polygonal Pyramid\n" +
+                "3. Polygonal Prism\n" +
+                "4. Sphere");
         int option = input.nextInt();
 
         switch (option) {
             case 1: Volume.cube(); break;
-            case 2: Volume.triangularPyramid(); break;
-            case 3: Volume.squarePyramid(); break;
+            case 2: Volume.polygonalPyramid(); break;
             case 4: Volume.polygonalPrism(); break;
+            case 5: Volume.sphere(); break;
             default: System.out.println("Invalid Input!");
         }
 
--- systems/Volume.java
+++ systems/Volume.java
@@ -13,27 +13,16 @@ public class Volume {
 
     }
 
-    public static void triangularPyramid () {
-        System.out.println("Enter your base side length: ");
+    public static void polygonalPyramid () {
+        System.out.println("Enter the number of sides: ");
+        int sides = input.nextInt();
+        System.out.println("Enter your base length: ");
         double baseLen = input.nextDouble();
-        System.out.println("Enter your base height: ");
-        double baseHeight = input.nextDouble();
-        double baseArea = baseLen * baseHeight / 2;
-
+        double triangleArea = (sides * Math.pow(baseLen, 2) / (4 * Math.tan(Math.PI/sides)));
         System.out.println("Enter your height: ");
         double height = input.nextDouble();
 
-        System.out.println("Your answer is: " + baseArea * height / 3);
-
-    }
-
-    public static void squarePyramid () {
-        System.out.println("Enter your base side length: ");
-        double baseLen = input.nextDouble();
-        System.out.println("Enter your height: ");
-        double height = input.nextDouble();
-
-        System.out.println("Your answer is: " + Math.pow(baseLen, 2) * height / 3);
+        System.out.println("Your answer is: " + height * triangleArea / 3);
 
     }
 
@@ -47,6 +36,20 @@ public class Volume {
         double height = input.nextDouble();
 
         System.out.println("Your answer is: " + height * triangleArea);
+
+    }
+
+    public static void sphere () {
+        System.out.println("Enter the radius: ");
+        double radius = input.nextDouble();
+
+        System.out.println("Would you like the answer in terms of PI? (y/n)");
+        String pi = input.next();
+
+        switch (pi) {
+            case "y": System.out.println("Your answer is: " + ((Math.pow(radius, 3) * 4) / 3) + "pi"); break;
+            case "n": System.out.println("Your answer is: " + (Math.pow(radius, 3) * 4 * Math.PI) / 3); break;
+        }
 
     }
 
